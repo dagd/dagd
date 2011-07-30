ca1ce6c981c0d7a231099f74ad116ec7d096155f
Ricky Elrod <ricky@elrod.me>
2011-07-30 02:31:38 -0400

    Some initial files. Exception handling works, routing does not.

---
+++ src/resources/global_resources.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is essentially stolen from Facebook's libphutil, but it allows
+// for doing: id(new Foo())->bar();  ... whereas (new Foo())->bar() is invalid
+// in php.
+function id($obj) {
+  return $obj;
+}
+
+function handleException(Exception $e) {
+  echo 'An error has occurred within dagd! Sorry about that!';
+}
+set_exception_handler('handleException');
---
+++ src/webroot/index.php
@@ -0,0 +1,7 @@
+<?php
+require dirname(dirname(__FILE__)).'/resources/global_resources.php';
+if (!$_GET['__path__']) {
+  throw new Exception(
+    'No __path__ GET variable was found.'.
+    'Your rewrite rules are incorrect!');
+}
