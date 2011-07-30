f3ff2d8c1b2767b8da141425febca956e4c2394f
Ricky Elrod <ricky@elrod.me>
2011-07-30 04:19:53 -0400

    Only the catch-all has to be last.

--- src/route.php
+++ src/route.php
@@ -1,11 +1,11 @@
 <?php
 require_once dirname(__FILE__).'/resources/global_resources.php';
 $routes = array(
+  '/$' => 'DaGdShortenHomeController',
   '/ip/?$' => 'DaGdIPController',
   '/w/(.+)/?$' => 'DaGdWhoisController',
   '/up/(.*)/?$' => 'DaGdUpController',
 
   // Keep this last, these are referenced in order.
-  '/$' => 'DaGdShortenHomeController',
-  #'/(.+)/?$' => 'DaGdShortenRedirectController',
+  // '/(.+)/?$' => 'DaGdShortenRedirectController',
 );
