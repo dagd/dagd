5f4a5699e84d129633230abc7e7c1c519c1a210a
Ricky Elrod <ricky@elrod.me>
2011-07-30 02:40:02 -0400

    require_once instead of require.

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -1,7 +1,8 @@
 <?php
-require dirname(dirname(__FILE__)).'/resources/global_resources.php';
+require_once dirname(dirname(__FILE__)).'/resources/global_resources.php';
 if (!$_GET['__path__']) {
   throw new Exception(
     'No __path__ GET variable was found.'.
     'Your rewrite rules are incorrect!');
 }
+
