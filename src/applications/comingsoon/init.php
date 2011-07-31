131e8207c4c33287fd829bd19c9047c95aaa2f05
Ricky Elrod <ricky@elrod.me>
2011-07-31 14:44:05 -0400

    remove old init.php files.

--- src/applications/comingsoon/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-class DaGdComingSoonController extends DaGdBaseClass {
-  public function render() {
-    return 'This page is coming soon!';
-  }
-}
--- src/applications/ip/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/../base/init.php';
-
-class DaGdIPController extends DaGdBaseClass {
-  public function render() {
-    return $_SERVER['REMOTE_ADDR'];
-  }
-}
