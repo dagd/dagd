d058cb81373e39d9146169c6eb39481af5ee7b41
Ricky Elrod <ricky@elrod.me>
2011-08-28 04:51:14 -0400

    Include a blank, working MySQL schema.

---
+++ blank_mysql_schema.sql
@@ -0,0 +1,80 @@
+-- MySQL dump 10.13  Distrib 5.1.52, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: dagd
+-- ------------------------------------------------------
+-- Server version	5.1.52
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shorturl_access`
+--
+
+DROP TABLE IF EXISTS `shorturl_access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shorturl_access` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `shorturl_id` int(10) unsigned NOT NULL,
+  `ip` varchar(45) NOT NULL,
+  `useragent` varchar(255) DEFAULT NULL,
+  `access_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `shorturl_id` (`shorturl_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shorturl_access`
+--
+
+LOCK TABLES `shorturl_access` WRITE;
+/*!40000 ALTER TABLE `shorturl_access` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shorturl_access` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shorturls`
+--
+
+DROP TABLE IF EXISTS `shorturls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shorturls` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `shorturl` varchar(10) NOT NULL DEFAULT '',
+  `longurl` text NOT NULL,
+  `owner_ip` varchar(45) NOT NULL,
+  `creation_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`shorturl`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shorturls`
+--
+
+LOCK TABLES `shorturls` WRITE;
+/*!40000 ALTER TABLE `shorturls` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shorturls` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-08-28  4:50:38
