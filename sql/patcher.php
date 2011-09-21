92c65b19fcbcf573a715bec84b6bf3de1553c988
Ricky Elrod <ricky@lucky01.(none)>
2011-09-21 19:05:49 -0400

    Make patcher.php confirm before it applies patches.

--- sql/patcher.php
+++ sql/patcher.php
@@ -2,29 +2,45 @@
 <?php
 require_once dirname(__FILE__).'/../src/resources/global_resources.php';
 
+$patch_number = 0;
 $current_schema = 0;
 if (file_exists(dirname(__FILE__).'/current_schema')) {
   $current_schema = (int)file_get_contents(dirname(__FILE__).'/current_schema');
 }
 
-$patch_number = 0;
+$patches_to_apply = array();
+$all_patches = glob(dirname(__FILE__).'/*.sql');
 
-foreach (glob(dirname(__FILE__).'/*.sql') as $patch) {
+// Loop through all the files, and find ones that need to be applied.
+foreach ($all_patches as $patch) {
   $patch_number = explode('sql/', $patch, 2);
   $patch_number = explode('.', $patch_number[1], 2);
   $patch_number = (int)$patch_number[0];
-  if ($patch_number <= $current_schema) {
-    continue;
+  if ($patch_number > $current_schema) {
+    $patches_to_apply[] = $patch;
   }
-  echo "Applying SQL patch {$patch}...\n";
-  $raw_query = file_get_contents($patch);
-  if ($__db_handler->multi_query($raw_query)) {
-    do {
-      if ($result = $__db_handler->store_result()) {
-        $result->free();
-      }
-    } while ($__db_handler->next_result());
+}
+
+if (count($patches_to_apply)) {
+  // Ask for a confirmation of what we're about to do.
+  echo "The following patches will be applied:\n";
+  echo implode("\n", $patches_to_apply)."\n";
+  echo "*** PRESS CTRL-C TO ABORT, OR ENTER TO CONTINUE ***";
+  fgets(STDIN);
+
+  foreach ($patches_to_apply as $patch) {
+    echo "Applying SQL patch {$patch}...\n";
+    $raw_query = file_get_contents($patch);
+    if ($__db_handler->multi_query($raw_query)) {
+      do {
+        if ($result = $__db_handler->store_result()) {
+          $result->free();
+        }
+      } while ($__db_handler->next_result());
+    }
   }
+} else {
+  echo "No patches to apply. :-)\n";
 }
 
 file_put_contents(dirname(__FILE__).'/current_schema', $patch_number++);
