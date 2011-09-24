a5f224f6c9fe6ef38c8f5b7fcd74a9271cf638ed
Ricky Elrod <ricky@lucky01.(none)>
2011-09-24 14:38:47 -0400

    This is basically a rewrite/total rework of DaGdShortenController. It's actually readable and commented a bit now. :o

--- src/applications/shorten/shorten.php
+++ src/applications/shorten/shorten.php
@@ -63,96 +63,125 @@ class DaGdShortenController extends DaGdBaseClass {
     }
   }
 
-  public function render() {
-    if (!array_key_exists('url', $_REQUEST)) {
-      if (count($this->route_matches) > 1) {
-        // Attempt to access a stored URL
-        $this->getLongURL();
-        if ($this->long_url) {
-          $this->logURLAccess();
-          header('X-Original-URL: '.$this->long_url);
-          header('Location: '.$this->long_url);
-        } else {
-          error404();
-          return;
-        }
+  private function redirect_from_shorturl() {
+    $this->getLongURL();
+    if ($this->long_url) {
+      $this->logURLAccess();
+      header('X-Original-URL: '.$this->long_url);
+      header('Location: '.$this->long_url);
+      return true;
+    } else {
+      error404();
+      return false;
+
+    }
+  }
+
+  private function store_shorturl() {
+    $query = $this->db_connection->prepare(
+      'INSERT INTO shorturls(shorturl, longurl, owner_ip) '.
+      'VALUES(?, ?, ?);');
+    $query->bind_param(
+      'sss',
+      $this->short_url,
+      $this->long_url,
+      $_SERVER['REMOTE_ADDR']);
+    
+    if ($query->execute()) {
+      return true;
+    } else {
+      error400('Something has gone wrong! :( ... Try again? Please?');
+      return false;
+    }
+  }
+
+  
+  private function set_shorturl_or_400() {
+    if ($short_url = request_or_default('shorturl')) {
+      $valid_char_pattern = '@^[\d\w-_]+$@i';
+      if (!preg_match($valid_char_pattern, $short_url)) {
+        error400('Invalid short URL entered. Alphanumeric only, please.');
+        return false;
       } else {
-        if (is_text_useragent()) {
-          return $this->help();
+        $this->short_url = substr($short_url, 0, 10);
+        if (!$this->isFreeShortURL()) {
+          error400('That custom URL was already taken, go back and try again!');
+          return false;
         }
-        
-        $content = '***da.gd***
+      }
+    } else {
+      $this->short_url = randstr(rand(4, 5));
+      while (!$this->isFreeShortURL()) {
+        $this->short_url = randstr(4, 5);
+      }
+    }
+
+    $this->short_url = htmlspecialchars(urlencode($this->short_url));
+    return true;
+  }
+
+  public function set_longurl_or_400() {
+    if ($_REQUEST['url'] == '') {
+      // If url was there but is an empty string, say so.
+      error400('Error: Cannot create something out of nothing.');
+      return false;
+    }
+    
+    if ($long_url = request_or_default('url')) {
+      // Something has at least been submitted. Is it valid?
+      if (preg_match('@^https?://@', $long_url)) {
+        // Good enough for now...probably needs some better checks.
+        $this->long_url = $long_url;
+        return true;
+      } else {
+        error400(
+          'Malformed original URL. Try again (http or https '.
+          'protocols only, please.).');
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  
+  public function render() {
+    if (array_key_exists('url', $_REQUEST)) {
+      if ($this->set_longurl_or_400() && $this->set_shorturl_or_400()) {
+        if ($this->store_shorturl()) {
+          header('X-Short-URL: '.$this->short_url);
+          $this->escape = false;
+          $new_link = DaGdConfig::get('general.baseurl').'/'.$this->short_url;
+          return '<a href="'.$new_link.'">'.$new_link.'</a>';
+        }
+      }
+      return;
+    }
+    
+    // No 'url' was passed, so we are not creating a new short-url.
+    if (count($this->route_matches) > 1) {
+      // Attempt to access a stored URL
+      $this->redirect_from_shorturl();
+      return;          
+    } else {
+      // We are not attempting to access a stored URL, but we also don't have
+      // a 'url' - Show the form so that we can create a new short-url.
+      if (is_text_useragent()) {
+        // No use in showing a form for text UAs. Rather, show help text.
+        return $this->help();
+      }
+
+      // Not a text useragent because we didn't return above.
+      // Bring in the form. // TODO: html in strings = bad.
+      $content = '***da.gd***
 <form method="POST" action="">
 Long URL: <input type="text" name="url" id="url" size="35" /><br />
 Optional custom suffix (truncated at 10 chars): <input type="text" name="shorturl" size="20" /><br />
 <input type="submit" value="Shorten URL" /><br />
 [help](/help) | [open source](http://github.com/codeblock/dagd)';
-        $markup = new DaGdMarkup($content);
-        $markup = $markup->render();
-        $markup .= '<script>window.onload = function() {document.getElementById("url").focus();}</script>';
-        echo $markup;
-      }
-    } else { // Submitted a URL
-
-      // TODO: Break this into its own function.
-      if ($short_url = request_or_default('shorturl')) {
-        $valid_char_pattern = '@^[\d\w-_]+$@i';
-        if (!preg_match($valid_char_pattern, $short_url)) {
-          error400('Invalid short URL entered. Alphanumeric only, please.');
-          return;
-        } else {
-          $this->short_url = substr($short_url, 0, 10);
-          if (!$this->isFreeShortURL()) {
-            error400('That custom URL was already taken, go back and try again!');
-            return;
-          }
-        }
-      } else {
-        $this->short_url = randstr(rand(4, 5));
-        while (!$this->isFreeShortURL()) {
-          $this->short_url = randstr(4, 5);
-        }
-      }
-
-      $this->short_url = htmlspecialchars(urlencode($this->short_url));
-      
-      if ($long_url = request_or_default('url')) {
-        // Something has at least been submitted. Is it valid?
-        if (preg_match('@^https?://@', $long_url)) {
-          // Good enough for now...probably needs some better checks.
-
-          $this->long_url = $long_url;
-          $this->long_url = $this->long_url;
-          
-          $query = $this->db_connection->prepare(
-            'INSERT INTO shorturls(shorturl, longurl, owner_ip) '.
-            'VALUES(?, ?, ?);');
-          $query->bind_param(
-            'sss',
-            $this->short_url,
-            $this->long_url,
-            $_SERVER['REMOTE_ADDR']);
-
-          if ($query->execute()) {
-            header('X-Short-URL: '.$this->short_url);
-            $this->escape = false;
-            $new_link = DaGdConfig::get('general.baseurl').'/'.$this->short_url;
-            return '<a href="'.$new_link.'">'.$new_link.'</a>';
-          } else {
-            error400('Something has gone wrong! :( ... Try again? Please?');
-            return;
-          }
-          
-        } else {
-          error400(
-            'Malformed original URL. Try again (http or https '.
-              'protocols only, please.).');
-          return;
-        }
-      } else {
-        error400('Error: Cannot create something out of nothing.');
-        return;
-      }
+      $markup = new DaGdMarkup($content);
+      $markup = $markup->render();
+      $markup .= '<script>window.onload = function() {document.getElementById("url").focus();}</script>';
+      echo $markup;
     }
   }
 }
