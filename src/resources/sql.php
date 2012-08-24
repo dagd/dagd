4bbd4210df7a00d245459d9ee4f422e4a9022986
Ricky Elrod <ricky@elrod.me>
2012-08-24 07:19:15 -0400

    Throw a detailed exception if mysqli isn't found. Fixes #11.

--- src/resources/sql.php
+++ src/resources/sql.php
@@ -1,6 +1,12 @@
 <?php
 
 // For better or worse, right now this is our gateway to the world of SQL.
+if (!function_exists('mysqli_connect')) {
+  throw new Exception(
+    'da.gd heavily depends on mysqli, and this library was not found. Please '.
+    'ensure that mysqli is installed, then restart your httpd and try again.');
+}
+
 $__db_handler = new mysqli(
     DaGdConfig::get('mysql.host'),
     DaGdConfig::get('mysql.user'),
