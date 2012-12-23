f763b092dd87b7b61aa60227d02040d558c4cd4e
Ricky Elrod <ricky@elrod.me>
2012-12-23 14:53:18 -0500

    Rewrite the test suite from scratch.

--- tests/dagd_test_suite.php
+++ tests/dagd_test_suite.php
@@ -2,8 +2,6 @@
 <?php
 // Sorry, this probably isn't as neat as you expected. But this is our test suite. :P
 
-echo "*** NOTE: This test suite expects to be run on a clean database. ***\n";
-
 define(
   'TEXT_UA',
   'curl/7.19.7 (x86_64-redhat-linux-gnu) libcurl/7.19.7 '.
@@ -14,238 +12,343 @@ define(
   'Mozilla/5.0 (X11; Linux x86_64; rv:9.0a1) Gecko/20110824 Firefox/9.0a1');
 
 require_once dirname(dirname(__FILE__)).'/src/resources/global_resources.php';
-$TEST_URL = DaGdConfig::get('general.baseurl');
-if (count($argv) > 1) {
-  $TEST_URL = $argv[1];
-}
 
-$path = null;
-$rx_headers = null;
-$tests_completed = array(
-  'attempted' => 0,
-  'successful' => 0,
-  'failed' => 0);
-
-function retrieve($test_path = '/', $ignore_errors = false) {
-  global $TEST_URL, $path, $rx_headers;
-  $rx_headers = null;
-  $path = $test_path;
-  $context = stream_context_create(
-    array(
-      'http' => array(
-        'ignore_errors' => $ignore_errors,
-      ),
-    )
+class DaGdTest {
+  public static $test_url;
+  protected $path;
+  protected $headers;
+  protected $tolerate_failure;
+  private $original_user_agent;
+  private static $results = array(
+    'pass' => 0,
+    'fail' => 0,
+    'tolerated fail' => 0,
   );
-  $obtain = @file_get_contents($TEST_URL.$path, false, $context);
-  $rx_headers = $http_response_header;
-  return strip_tags($obtain);
-}
 
-function test_response_code($test_path, $expected_code) {
-  global $rx_headers;
-  retrieve($test_path);
-  $match = preg_match('@'.$expected_code.'@', $rx_headers[0]);
-  return test(
-    $match,
-    'give correct HTTP Response code ('.$expected_code.' ?= '.
-    $rx_headers[0].')');
-}
+  public function setTolerateFailure($tolerate_failure) {
+    $this->tolerate_failure = $tolerate_failure;
+    return $this;
+  }
 
-function test_content_type($test_path, $expected_mimetype, $useragent) {
-  global $rx_headers;
-  $headers = array();
+  public function setUserAgent($user_agent) {
+    $this->original_user_agent = ini_get('user_agent');
+    ini_set('user_agent', $user_agent);
+    return $this;
+  }
 
-  // Store the original useragent so we can revert.
-  $orig_useragent = ini_get('user_agent');
+  public static function getResultsSummary() {
+    return DaGdTest::$results;
+  }
 
-  ini_set('user_agent', $useragent);
-
-  // Pull content so that $http_response_header gets updated.
-  retrieve($test_path);
-
-  // Restore
-  ini_set('user_agent', $orig_useragent);
-
-  // Create an associative array based on $http_response_header.
-  foreach ($rx_headers as $header) {
-    $header_split = explode(': ', $header, 2);
-    if (count($header_split) > 1) {
-      list($name, $value) = $header_split;
-      $headers[$name] = $value;
+  protected function fail($text, $exit = false) {
+    if ($this->tolerate_failure) {
+      echo chr(27)."[1;33m".'*** (tolerable failure) '.$text.' ***'.chr(27)."[0m"."\n";
+      DaGdTest::$results['tolerated fail']++;
+    } else {
+      echo chr(27)."[1;31m".'*** '.$text.' ***'.chr(27)."[0m"."\n";
+      DaGdTest::$results['fail']++;
+    }
+    if ($exit && !$this->tolerate_failure) {
+      echo "Exiting with status code '1' - to bypass a git pre-commit hook,\n";
+      echo "pass `--no-verify` to your `git commit` command.\n";
+      exit(1);
     }
   }
-  $correctness = strstr($headers['Content-Type'], $expected_mimetype);
-  return test($correctness, 'return correct mimetype ('.$expected_mimetype.')');
-}
 
-function test_regex($test_path, $pattern, $not_include = false) {
-  $content = retrieve($test_path, true);
-  if ($not_include) {
-    $match = !preg_match($pattern, $content);
-    return test($match, 'must NOT match pattern: '.$pattern);
-  } else {
-    $match = preg_match($pattern, $content);
-    return test($match, 'must match pattern: '.$pattern);
+  protected function pass($text, $exit = false) {
+    echo chr(27)."[1;32m".'*** '.$text.' ***'.chr(27)."[0m"."\n";
+    DaGdTest::$results['pass']++;
+    if ($exit) {
+      exit(0);
+    }
+  }
+
+  protected function retrieve($ignore_errors = false) {
+    $context = stream_context_create(
+      array(
+        'http' => array(
+          'ignore_errors' => $ignore_errors,
+        ),
+      )
+    );
+    $obtain = @file_get_contents($this::$test_url.$this->path, false, $context);
+    $this->headers = $http_response_header;
+    if ($this->original_user_agent) {
+      ini_set('user_agent', $this->original_user_agent);
+    }
+    return strip_tags($obtain);
+  }
+
+  protected function getHeaders() {
+    if (!$this->headers) {
+      throw new Exception(
+        'Call retrieve() before calling getHeaders()!');
+    }
+    return $this->headers;
+  }
+
+  protected function test($condition, $summary) {
+    if ($condition) {
+      $this->pass('Test PASSED ['.$this->path.']: '.$summary);
+      return true;
+    } else {
+      $this->fail('Test FAILED ['.$this->path.']: '.$summary);
+      if ($this->tolerate_failure) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
 
-function fail($text, $exit = false) {
-  echo chr(27)."[1;31m".'*** '.$text.' ***'.chr(27)."[0m"."\n";
-  if ($exit) {
-    echo "Exiting with status code '1' - to bypass a git pre-commit hook,\n";
-    echo "pass `--no-verify` to your `git commit` command.\n";
-    exit(1);
+final class DaGdResponseCodeTest extends DaGdTest {
+  private $expected_code;
+
+  public function __construct($test_path, $expected_code) {
+    $this->path = $test_path;
+    $this->expected_code = $expected_code;
+  }
+
+  public function run() {
+    $this->retrieve();
+    $headers = $this->getHeaders();
+    $match = preg_match('@'.$this->expected_code.'@', $headers[0]);
+    return $this->test(
+      $match,
+      'give correct HTTP Response code ('.$this->expected_code.' ?= '.
+      $headers[0].')');
   }
 }
 
-function pass($text, $exit = false) {
-  echo chr(27)."[1;32m".'*** '.$text.' ***'.chr(27)."[0m"."\n";
-  if ($exit) {
-    exit(0);
+final class DaGdContentTypeTest extends DaGdTest {
+  private $expected_mimetype;
+
+  public function __construct($test_path, $expected_mimetype) {
+    $this->path = $test_path;
+    $this->expected_mimetype = $expected_mimetype;
+  }
+
+  public function run() {
+
+    $this->retrieve();
+    $headers = $this->getHeaders();
+
+    foreach ($headers as $header) {
+      $header_split = explode(': ', $header, 2);
+      if (count($header_split) > 1) {
+        list($name, $value) = $header_split;
+        $headers[$name] = $value;
+      }
+    }
+    $correctness = strstr($headers['Content-Type'], $this->expected_mimetype);
+    return $this->test(
+      $correctness,
+      'return correct mimetype ('.$this->expected_mimetype.')');
   }
 }
 
-function test($condition, $summary) {
-  global $path, $tests_completed;
-  $tests_completed['attempted']++;
-  if ($condition) {
-    pass('Test PASSED ['.$path.']: '.$summary);
-    $tests_completed['successful']++;
-    return true;
-  } else {
-    fail('Test FAILED ['.$path.']: '.$summary);
-    $tests_completed['failed']++;
-    return false;
+final class DaGdRegexTest extends DaGdTest {
+  private $regex;
+  private $invert;
+
+  public function __construct($test_path, $pattern, $invert = false) {
+    $this->path = $test_path;
+    $this->regex = $pattern;
+    $this->invert = $invert;
+  }
+
+  public function run() {
+    $content = $this->retrieve(true);
+    if ($this->invert) {
+      $match = !preg_match($this->regex, $content);
+      return $this->test($match, 'must NOT match pattern: '.$this->regex);
+    } else {
+      $match = preg_match($this->regex, $content);
+      return $this->test($match, 'must match pattern: '.$this->regex);
+    }
   }
 }
+
+function id($a) {
+  return $a;
+}
+
+DaGdTest::$test_url = 'http://dagd.local/';
 
 /*********** / ***********/
 
-// Ensure that the site itself loads at all.
-$content = retrieve();
-test($content, 'retrieve /');
-
-test_content_type('/', 'text/plain', TEXT_UA);
-test_content_type('/', 'text/html', FIREFOX_UA);
-
-// Check if the site contains the string 'Current commands'.
-test_regex('/', '@various conditions@');
+id(new DaGdContentTypeTest('/', 'text/plain'))
+  ->setUserAgent(TEXT_UA)
+  ->run();
+id(new DaGdContentTypeTest('/', 'text/html'))
+  ->setUserAgent(FIREFOX_UA)
+  ->run();
+id(new DaGdRegexTest('/', '@various conditions@'))
+  ->run();
 
 
 /*********** /ip ***********/
 
 // See if /ip contains a number followed by a '.'
-test_regex('/ip', '@[0-9]\.@');
-
+id(new DaGdRegexTest('/ip', '@[0-9]\.@'))
+  ->run();
 
 /*********** /wp/Phuzion ***********/
 
-// Make sure response is numeric only.
-test_regex('/ec/Phuzion', '@^[0-9]+$@');
-test_regex('/ec/Phuzion?lang=en', '@^[0-9]+$@');
-test_regex('/ec/Phuzion?lang=fr', '@^[0-9]+$@');
-test_response_code('/ec/Phuzion?lang=asdfasdf', 400);
-test_response_code('/ec/Phuzion?lang=asd123dsa', 400);
+id(new DaGdRegexTest('/ec/Phuzion', '@^[0-9]+$@'))
+  ->run();
+id(new DaGdRegexTest('/ec/Phuzion?lang=en', '@^[0-9]+$@'))
+  ->run();
+id(new DaGdRegexTest('/ec/Phuzion?lang=fr', '@^[0-9]+$@'))
+  ->run();
+id(new DaGdResponseCodeTest('/ec/Phuzion?lang=asdfasdf', 400))
+  ->run();
+
+/************ /w/xxxxxxx ************/
+
+/* // Ensure that whois is functioning. */
+id(new DaGdRegexTest('/w/google.com', '@Mountain View@'))
+  ->run();
+id(new DaGdRegexTest('/w/da.gd', '@Ricky@'))
+  ->run();
+id(new DaGdRegexTest('/w/4.2.2.2', '@Level 3 Communications@'))
+  ->run();
+
+/************ /headers/xxxxxxx ************/
+
+id(new DaGdRegexTest('/headers', '@Host: @'))
+  ->run();
+id(new DaGdRegexTest('/headers', '@X-DaGd-Proxy: @', true))
+  ->run();
+id(new DaGdRegexTest('/headers/google.com', '@Server: gws@'))
+  ->run();
+id(new DaGdRegexTest('/headers/http://google.com/', '@Server: gws@'))
+  ->run();
+
+/************ /up/xxxxxxx ************/
+id(new DaGdRegexTest('/up/google.com', '@^200$@'))
+  ->run();
+id(new DaGdRegexTest('/up/https://encrypted.google.com/', '@^200$@'))
+  ->run();
+id(new DaGdRegexTest('/up/http://google.com/404', '@^404$@'))
+  ->run();
+
+/************ /et/xxxxxxx ************/
+id(new DaGdResponseCodeTest('/et/750009720', 302))
+  ->run();
+
+/************ /host/xxxxxxx ************/
+id(new DaGdRegexTest('/host/google.com', '@2607:f8b0@'))
+  ->run();
+id(new DaGdRegexTest('/host/google.com', '@74\.125@'))
+  ->run();
+id(new DaGdRegexTest('/host/google.com?noipv6', '@:@', true))
+  ->run();
+id(new DaGdRegexTest('/host/69.171.237.16', '@facebook.com@'))
+  ->run();
+
+/************ /break/ ************/
+id(new DaGdResponseCodeTest('/break', 500))
+  ->run();
+id(new DaGdRegexTest('/break', '@An error has occurred@'))
+  ->run();
+
+/************ /c/store/xxxxxxx/xxxxxxx ************/
+id(
+  new DaGdResponseCodeTest(
+    '/c/store/g/https://www.google.com/search%3Fq%3D$PARAMETERS',
+    200))
+  ->setTolerateFailure(true)
+  ->run();
+id(
+  new DaGdResponseCodeTest(
+    '/c/store/g/https://www.google.com/search%3Fq%3D$PARAMETERS',
+    400))
+  ->run();
+id(
+  new DaGdRegexTest(
+    '/c/store/g1/https://www.google.com/search%3Fq%3D$PARAMETERS',
+    '@Success@'))
+  ->setTolerateFailure(true)
+  ->run();
+id(
+  new DaGdRegexTest(
+    '/c/store/g1/https://www.google.com/search%3Fq%3D$PARAMETERS',
+    '@already been defined@'))
+  ->run();
 
 
-/*********** /w/xxxxxxx ***********/
+/************ /c/xxxxxxx/xxxxxxx ************/
+id(new DaGdResponseCodeTest('/c/g/foobar', 302))
+  ->run();
+id(new DaGdResponseCodeTest('/c/g%20foobar', 302))
+  ->run();
+id(new DaGdResponseCodeTest('/c/nonexistent/foobar', 400))
+  ->run();
+id(new DaGdRegexTest('/c/nonexistent/foobar', '@was not found@'))
+  ->run();
 
-// Ensure that whois is functioning.
-test_regex('/w/google.com', '@Mountain View@');
-test_regex('/w/fbco.de', '@Menlo Park@');
-test_regex('/w/da.gd', '@Ricky@');
-test_regex('/w/4.2.2.2', '@Level 3 Communications@');
+/************ /c/[xxxxxxx] ************/
+id(new DaGdRegexTest('/c/', '@Redirect@'))
+  ->run();
+id(new DaGdRegexTest('/c/json', '@{"g":@'))
+  ->run();
+id(new DaGdRegexTest('/c/json/', '@"g1":@'))
+  ->run();
+id(new DaGdContentTypeTest('/c/json/', 'application/json'))
+  ->setUserAgent(TEXT_UA)
+  ->run();
+id(new DaGdContentTypeTest('/c/json', 'application/json'))
+  ->setUserAgent(FIREFOX_UA)
+  ->run();
+id(new DaGdContentTypeTest('/c', 'text/html'))
+  ->setUserAgent(FIREFOX_UA)
+  ->run();
+id(new DaGdContentTypeTest('/c/', 'text/html'))
+  ->setUserAgent(FIREFOX_UA)
+  ->run();
+id(new DaGdContentTypeTest('/c', 'text/plain'))
+  ->setUserAgent(TEXT_UA)
+  ->run();
+id(new DaGdContentTypeTest('/c/', 'text/plain'))
+  ->setUserAgent(TEXT_UA)
+  ->run();
 
+/************ /image/xxxxxxx/[xxxxxxx] ************/
+id(new DaGdContentTypeTest('/image/200x200', 'image/png'))
+  ->setUserAgent(FIREFOX_UA)
+  ->run();
+id(new DaGdContentTypeTest('/image/200x200', 'image/png'))
+  ->setUserAgent(TEXT_UA)
+  ->run();
+id(new DaGdContentTypeTest('/image/10x10.jpg', 'image/jpeg'))
+  ->setUserAgent(TEXT_UA)
+  ->run();
+id(new DaGdContentTypeTest('/image/30x20.gif?bgcolor=333333', 'image/gif'))
+  ->setUserAgent(FIREFOX_UA)
+  ->run();
+id(new DaGdResponseCodeTest('/image/300', 400))
+  ->run();
+id(new DaGdResponseCodeTest('/image/300000000x1212121221', 400))
+  ->run();
 
-/*********** /headers/xxxxxxx ***********/
+/************ /status/xxxxxxx/[xxxxxxx] ************/
+id(new DaGdResponseCodeTest('/status/400', 400))
+  ->run();
+id(new DaGdResponseCodeTest('/status/403', 403))
+  ->run();
+id(new DaGdResponseCodeTest('/status/123/hi', 123))
+  ->run();
 
-// Ensure that headers are seen.
-test_regex('/headers/', '@Host: @');
-test_regex('/headers/', '@X-DaGd-Proxy:@', true);
-test_regex('/headers/google.com', '@Server: gws@');
-test_regex('/headers/http://google.com/', '@Server: gws@');
+/************ /isp/[xxxxxxx] ************/
+id(new DaGdRegexTest('/isp/127.0.0.1', '@^Internet Assigned Numbers Authority@'))
+  ->run();
+id(new DaGdRegexTest('/isp/69.171.237.16', '@^Facebook, Inc\.$@'))
+  ->run();
 
-
-/*********** /up/xxxxxxx ***********/
-
-test_regex('/up/google.com', '@^200$@');
-test_regex('/up/https://encrypted.google.com', '@^200$@');
-
-
-/*********** /et/xxxxxxx ***********/
-test_response_code('/et/75009720', 302);
-
-
-/*********** /host/xxxxxxx ***********/
-test_regex('/host/google.com', '@2607:f8b0@');
-test_regex('/host/google.com', '@74\.125@');
-test_regex('/host/facebook.com?noipv6', '@face:b00c@', true);
-test_regex('/host/69.171.237.16', '@facebook.com@');
-
-/*********** /break/ ***********/
-test_response_code('/break', 500);
-test_regex('/break', '@An error has occurred@');
-
-/*********** /c/store/xxxxxxx/xxxxxxx ***********/
-test_response_code(
-  '/c/store/g/https://www.google.com/search%3Fq%3D$PARAMETERS',
-  200);
-test_response_code(
-  '/c/store/g/https://www.google.com/search%3Fq%3D$PARAMETERS',
-  400);
-test_regex(
-  '/c/store/g1/https://www.google.com/search%3Fq%3D$PARAMETERS',
-  '@Success@');
-test_regex(
-  '/c/store/g1/https://www.google.com/search%3Fq%3D$PARAMETERS',
-  '@already been defined@');
-
-/*********** /c/xxxxxxx/xxxxxxx ***********/
-test_response_code('/c/g/foobar', 302);
-test_response_code('/c/g%20foobar', 302);
-test_response_code('/c/nonexistent/foobar', 400);
-test_regex('/c/nonexistent/foobar', '@was not found@');
-
-/*********** /c/[xxxxxxx] ***********/
-test_regex('/c/', '@Redirect@');
-test_regex('/c/json', '@{"g":@');
-test_regex('/c/json/', '@"g1":@');
-test_content_type('/c/json/', 'application/json', TEXT_UA);
-test_content_type('/c/json/', 'application/json', FIREFOX_UA);
-test_content_type('/c/json', 'application/json', TEXT_UA);
-test_content_type('/c/json', 'application/json', FIREFOX_UA);
-test_content_type('/c', 'text/html', FIREFOX_UA);
-test_content_type('/c/', 'text/html', FIREFOX_UA);
-test_content_type('/c', 'text/plain', TEXT_UA);
-test_content_type('/c/', 'text/plain', TEXT_UA);
-
-/*********** /image/xxxxxxx/[xxxxxxx] ***********/
-test_content_type('/image/200x200/', 'image/png', TEXT_UA);
-test_content_type('/image/200x200/', 'image/png', FIREFOX_UA);
-test_content_type('/image/400x100/jpeg/', 'image/jpeg', TEXT_UA);
-test_content_type('/image/400x100/jpg/', 'image/jpeg', TEXT_UA);
-test_content_type('/image/400x100/jpeg', 'image/jpeg', TEXT_UA);
-test_content_type('/image/400x100', 'image/png', TEXT_UA);
-test_content_type('/image/400x100?bgcolor=444444', 'image/png', TEXT_UA);
-test_content_type('/image/400x100.gif?bgcolor=444444', 'image/gif', TEXT_UA);
-test_content_type('/image/400x100/gif', 'image/gif', FIREFOX_UA);
-test_response_code('/image/300', 400);
-test_response_code('/image/3000000000x120102102', 400);
-
-/*********** /status/xxxxxxx/[xxxxxxx] ***********/
-test_response_code('/status/404', 404);
-test_response_code('/status/605', 605);
-test_response_code('/status/123/hi', 123);
-
-/*********** /isp/[xxxxxxx] ***********/
-test_regex(
-  '/isp/127.0.0.1',
-  '@Internet Assigned Numbers Authority@');
-test_regex(
-  '/isp/69.171.237.16',
-  '@Facebook@');
-
-echo "Report: Completed {$tests_completed['attempted']} tests.\n";
-echo "Report: {$tests_completed['successful']} were successful.\n";
-echo "Report: {$tests_completed['failed']} failed.\n";
+$results = DaGdTest::getResultsSummary();
+echo chr(27)."[1;32m Passed        : ".$results['pass'].chr(27)."[0m"."\n";
+echo chr(27)."[1;31m Failed        : ".$results['fail'].chr(27)."[0m"."\n";
+echo chr(27)."[1;33m Tolerated Fail: ".$results['tolerated fail'].chr(27)."[0m".
+  "\n";
