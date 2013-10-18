fbdd10364ae1e6a138a0d4428550a605146d22e7
Ricky Elrod <ricky@elrod.me>
2013-10-18 05:25:33 -0400

    Port over a rough, but working, copy of Image.

--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,12 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
 
+import qualified Data.ByteString.Char8 as BC8
+import qualified Data.ByteString.Lazy as BL
 import Data.List (isInfixOf)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (mappend, mconcat)
+import qualified Data.Text as TS
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Encoding as TE
 
@@ -17,11 +20,19 @@ import Network.Wai.Middleware.Gzip
 import Network.Whois
 import qualified Network.Socket as S
 
+import Graphics.ImageMagick.MagickWand
+
 import qualified Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 import Web.Scotty
+
+safe :: ActionM a -> ActionM (Maybe a)
+safe = (`rescue` const (return Nothing)) . (Just `fmap`)
+
+paramMay :: (Parsable a) => T.Text -> ActionM (Maybe a)
+paramMay = safe . param
 
 isTextUseragent :: Maybe String -> Bool
 isTextUseragent (Just a) = any (`isInfixOf` a) textUAs
@@ -74,3 +85,34 @@ main = scotty 3000 $ do
     item <- param "item"
     qs <- fmap (T.fromStrict . TE.decodeUtf8 . rawQueryString) request
     redirect $ "http://www.etsy.com/listing/" `mappend` item `mappend` qs
+
+  get (regex "^/image/([0-9]+)[x*]([0-9]+)\\.([[:alnum:]]+)$") $ do
+    width <- param "1"
+    height <- param "2"
+    extension <- param "3" :: ActionM String
+    bgColor <- paramMay "bgcolor"
+    text <- paramMay "text"
+
+    img <- liftIO $ withMagickWandGenesis $ do
+      (_,w) <- magickWand
+      (_,dw) <- drawingWand
+      c <- pixelWand
+      c `setColor` BC8.append (BC8.pack "#") (fromMaybe "333333" bgColor)
+      newImage w width height c
+      -- Texty stuff.
+      c `setColor` "white"
+      dw `setFillColor` c
+      dw `setTextAntialias` True
+      dw `setStrokeOpacity` 0
+
+      let m = fromMaybe (show width ++ "x" ++ show height) text
+      drawAnnotation dw 65 65 (TS.pack m)
+
+      -- Do housekeeping and return the image.
+      drawImage w dw
+      w `setImageFormat` TS.pack extension
+      getImageBlob w
+
+    mime <- liftIO $ withMagickWandGenesis $ toMime (TS.pack extension)
+    setHeader "Content-Type" (T.fromStrict mime)
+    raw $ BL.fromStrict img
