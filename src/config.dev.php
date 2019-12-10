d04ad85d4f9e07e007aa188934c770b780cf9d9f
Rick Elrod <rick@elrod.me>
2019-12-10 02:54:53 -0600

    [config] default statsd off
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- src/config.dev.php
+++ src/config.dev.php
@@ -41,7 +41,7 @@ class DaGdConfig {
     ),
 
     // Control statsd metrics collection.
-    'general.statsd' => true,
+    'general.statsd' => false,
 
     'general.statsd_host' => 'localhost',
 
