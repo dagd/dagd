e0eb38d1673a9110272fc894bb0d16a8ff703af2
Rick Elrod <rick@elrod.me>
2020-04-12 09:23:34 -0500

    redesign test suite a bit, make it concurrent, and dust of tests
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- tests/dagd_test_suite.php
+++ tests/dagd_test_suite.php
@@ -4,23 +4,133 @@
 
 require_once dirname(dirname(__FILE__)).'/src/resources/global_resources.php';
 
-class DaGdTest {
+define('SUCCESS', 0);
+define('FAILURE', 1);
+define('TOLERATED_FAILURE', 2);
+
+class DaGdTestRunner {
+  private $concurrency = 5;
+  private $pids = array();
+  private $tests = array();
+  private $last_started = 0;
+  private $return_code = 0;
+
+  private $passes = 0;
+  private $tolerated_failures = 0;
+  private $failures = 0;
+
+  public function getPasses() {
+    return $this->passes;
+  }
+
+  public function getToleratedFailures() {
+    return $this->tolerated_failures;
+  }
+
+  public function getFailures() {
+    return $this->failures;
+  }
+
+  public function getReturnCode() {
+    return $this->return_code;
+  }
+
+  public function arm($test) {
+    $this->tests[] = $test;
+    return $this;
+  }
+
+  public function setConcurrency($int) {
+    $this->concurrency = $int;
+    return $this;
+  }
+
+  private function handleRC($rc) {
+    switch ($rc) {
+    case SUCCESS:
+      $this->passes++;
+      break;
+    case TOLERATED_FAILURE:
+      $this->tolerated_failures++;
+      break;
+    default:
+      $this->return_code = 1;
+      $this->failures++;
+    }
+  }
+
+  public function run($start_test = 0) {
+    // Run preparatory tests first. These have to be done in a particular
+    // order, so we can't just throw them to the dogs...er, child processes.
+    $remaining_tests = array();
+    foreach ($this->tests as $test) {
+      if ($test->getPreparatory()) {
+        $rc = $test->run();
+        $this->handleRC($rc);
+      } else {
+        $remaining_tests[] = $test;
+      }
+    }
+
+    // We do this a bit "oddly" -- split up the tests into $concurrency
+    // chunks and spawn children that work on those chunks.
+    $num_chunks = ceil(count($remaining_tests) / $this->concurrency);
+    $chunks = array_chunk($remaining_tests, $num_chunks);
+
+    foreach ($chunks as $chunk) {
+      $pid = pcntl_fork();
+      if ($pid === -1) {
+        echo 'Could not fork child thread.';
+        exit(1);
+      }
+      if ($pid === 0) {
+        $exit = SUCCESS;
+        foreach ($chunk as $test) {
+          $rc = $test->run();
+          if ($rc != SUCCESS) {
+            $exit = $rc;
+          }
+        }
+        exit($exit);
+      } else {
+        $this->pids[] = $pid;
+      }
+    }
+
+    foreach ($this->pids as $pid) {
+      $status = null;
+      pcntl_waitpid($pid, $status);
+      $rc = pcntl_wexitstatus($status);
+      $this->handleRC($rc);
+    }
+
+    return $this->return_code;
+  }
+}
+
+abstract class DaGdTest {
   public static $test_url;
   protected $path;
   protected $headers;
   protected $tolerate_failure;
   protected $accept = '*/*';
   private $original_user_agent;
+  private $preparatory = false;
 
-  private static $results = array(
-    'pass' => 0,
-    'fail' => 0,
-    'tolerated fail' => 0,
-  );
+  abstract public function run();
 
   public function setTolerateFailure($tolerate_failure) {
     $this->tolerate_failure = $tolerate_failure;
     return $this;
+  }
+
+  public function setPreparatory($is_preparatory) {
+    $this->preparatory = $is_preparatory;
+    return $this;
+  }
+
+  public function getPreparatory() {
+    return $this->preparatory;
   }
 
   public function setUserAgent($user_agent) {
@@ -34,17 +144,11 @@ class DaGdTest {
     return $this;
   }
 
-  public static function getResultsSummary() {
-    return DaGdTest::$results;
-  }
-
   protected function fail($text, $exit = false) {
     if ($this->tolerate_failure) {
       echo chr(27)."[1;33m".'*** (tolerable failure) '.$text.' ***'.chr(27)."[0m"."\n";
-      DaGdTest::$results['tolerated fail']++;
     } else {
       echo chr(27)."[1;31m".'*** '.$text.' ***'.chr(27)."[0m"."\n";
-      DaGdTest::$results['fail']++;
     }
     if ($exit && !$this->tolerate_failure) {
       echo "Exiting with status code '1' - to bypass a git pre-commit hook,\n";
@@ -55,7 +159,6 @@ class DaGdTest {
 
   protected function pass($text, $exit = false) {
     echo chr(27)."[1;32m".'*** '.$text.' ***'.chr(27)."[0m"."\n";
-    DaGdTest::$results['pass']++;
     if ($exit) {
       exit(0);
     }
@@ -91,13 +194,13 @@ class DaGdTest {
   protected function test($condition, $summary) {
     if ($condition) {
       $this->pass('Test PASSED ['.$this->path.']: '.$summary);
-      return true;
+      return SUCCESS;
     } else {
       $this->fail('Test FAILED ['.$this->path.']: '.$summary);
       if ($this->tolerate_failure) {
-        return true;
+        return TOLERATED_FAILURE;
       } else {
-        return false;
+        return FAILURE;
       }
     }
   }
@@ -181,269 +284,289 @@ if (count($argv) > 1) {
   DaGdTest::$test_url = 'http://dagd.local/';
 }
 
+$runner = new DaGdTestRunner();
+
 /*********** / ***********/
+$runner->arm(
+  id(new DaGdContentTypeTest('/', 'text/plain'))
+    ->setAccept('text/plain'));
 
-id(new DaGdContentTypeTest('/', 'text/plain'))
-  ->setAccept('text/plain')
-  ->run();
-id(new DaGdContentTypeTest('/', 'text/html'))
-  ->setAccept('text/html')
-  ->run();
-id(new DaGdRegexTest('/', '@various conditions@'))
-  ->run();
+$runner->arm(
+  id(new DaGdContentTypeTest('/', 'text/html'))
+    ->setAccept('text/html'));
 
+$runner->arm(
+  id(new DaGdRegexTest('/', '@various conditions@')));
 
 /*********** /ip ***********/
 
 // See if /ip contains a number followed by a '.'
-id(new DaGdRegexTest('/ip', '@[0-9]\.@'))
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/ip', '@[0-9]\.@')));
 
 /*********** /ec/CodeBlock ***********/
 
-id(new DaGdRegexTest('/ec/CodeBlock', '@^[0-9]+$@'))
-  ->run();
-id(new DaGdRegexTest('/ec/CodeBlock?lang=en', '@^[0-9]+$@'))
-  ->run();
-id(new DaGdRegexTest('/ec/CodeBlock?lang=fr', '@^[0-9]+$@'))
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/ec/CodeBlock', '@^[0-9]+$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/ec/CodeBlock?lang=en', '@^[0-9]+$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/ec/CodeBlock?lang=fr', '@^[0-9]+$@')));
 
 /************ /w/xxxxxxx ************/
 
 /* // Ensure that whois is functioning. */
-id(new DaGdRegexTest('/w/google.com', '@ns1.google.com@'))
-  ->run();
-id(new DaGdRegexTest('/w/da.gd', '@bill.ns.cloudflare.com@'))
-  ->run();
-id(new DaGdRegexTest('/w/4.2.2.2', '@Level 3 Parent@'))
-  ->run();
-id(new DaGdRegexTest('/w/74.94.18.108', '@OrgAbusePhone@'))
-  ->run();
-id(
-  new DaGdRegexTest(
-    '/w/2001:470:8:624:211c:aaaa:1111:1111',
-    '@Hurricane Electric@'))
-  ->run();
-id(
-  new DaGdRegexTest(
-    '/w/2001:470:8:624:211c:aaaa:1111:1111',
-    '@Hurricane Electric@'))
-  ->run();
-id(
-  new DaGdRegexTest(
-    '/w/trees.network',
-    '@Registrar WHOIS Server: whois.1api.net@'))
-  ->run();
-id(new DaGdRegexTest('/w/trees.network','@1API GmbH@'))
-  ->run();
-id(
-  new DaGdRegexTest(
-    '/w/donuts.co',
-    '@Registrant Organization: Donuts Inc.@'))
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/w/google.com', '@ns1.google.com@')));
+$runner->arm(
+  id(new DaGdRegexTest('/w/da.gd', '@bill.ns.cloudflare.com@i')));
+$runner->arm(
+  id(new DaGdRegexTest('/w/4.2.2.2', '@Level 3 Parent@')));
+$runner->arm(
+  id(new DaGdRegexTest('/w/74.94.18.108', '@OrgAbusePhone@')));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/w/2001:470:8:624:211c:aaaa:1111:1111',
+      '@Hurricane Electric@')));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/w/2001:470:8:624:211c:aaaa:1111:1111',
+      '@Hurricane Electric@')));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/w/trees.network',
+      '@Registrar WHOIS Server: whois.1api.net@')));
+$runner->arm(
+  id(new DaGdRegexTest('/w/trees.network','@1API GmbH@')));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/w/donuts.co',
+      '@Registrant Organization: Donuts Inc.@')));
 
 /************ /headers/xxxxxxx ************/
 
-id(new DaGdRegexTest('/headers', '@Host: @'))
-  ->run();
-id(new DaGdRegexTest('/headers', '@X-DaGd-Proxy: @', true))
-  ->run();
-id(new DaGdRegexTest('/headers/google.com', '@Server: gws@'))
-  ->run();
-id(new DaGdRegexTest('/headers/http://google.com/', '@Server: gws@'))
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/headers', '@Host: @')));
+$runner->arm(
+  id(new DaGdRegexTest('/headers', '@X-DaGd-Proxy: @', true)));
+$runner->arm(
+  id(new DaGdRegexTest('/headers/google.com', '@Server: gws@')));
+$runner->arm(
+  id(new DaGdRegexTest('/headers/http://google.com/', '@Server: gws@')));
 
 /************ /up/xxxxxxx ************/
-id(new DaGdRegexTest('/up/google.com', '@^200$@'))
-  ->run();
-id(new DaGdRegexTest('/up/https://encrypted.google.com/', '@^200$@'))
-  ->run();
-id(new DaGdRegexTest('/up/http://google.com/404', '@^404$@'))
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/up/google.com', '@^200$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/up/https://encrypted.google.com/', '@^200$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/up/http://google.com/404', '@^404$@')));
 
 /************ /et/xxxxxxx ************/
-id(new DaGdResponseCodeTest('/et/750009720', 302))
-  ->run();
+$runner->arm(
+  id(new DaGdResponseCodeTest('/et/750009720', 302)));
 
 /************ /host/xxxxxxx ************/
-id(new DaGdRegexTest('/host/ipv6.google.com', '@:@'))
-  ->run();
-id(new DaGdRegexTest('/host/google.com', '@[0-9]\.@'))
-  ->run();
-id(new DaGdRegexTest('/host/google.com?noipv6', '@:@', true))
-  ->run();
-id(new DaGdRegexTest('/host/4.2.2.2', '@b.resolvers.Level3.net@'))
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/host/ipv6.google.com', '@:@')));
+$runner->arm(
+  id(new DaGdRegexTest('/host/google.com', '@[0-9]\.@')));
+$runner->arm(
+  id(new DaGdRegexTest('/host/google.com?noipv6', '@:@', true)));
+$runner->arm(
+  id(new DaGdRegexTest('/host/4.2.2.2', '@b.resolvers.Level3.net@')));
 
 /************ /break/ ************/
-id(new DaGdResponseCodeTest('/break', 500))
-  ->run();
-id(new DaGdRegexTest('/break', '@An error has occurred@'))
-  ->run();
+$runner->arm(
+  id(new DaGdResponseCodeTest('/break', 500)));
+$runner->arm(
+  id(new DaGdRegexTest('/break', '@An error has occurred@')));
 
 /************ /c/store/xxxxxxx/xxxxxxx ************/
-id(
-  new DaGdResponseCodeTest(
-    '/c/store/g/https://www.google.com/search%3Fq%3D$PARAMETERS',
-    200))
-  ->setTolerateFailure(true)
-  ->run();
-id(
-  new DaGdResponseCodeTest(
-    '/c/store/g/https://www.google.com/search%3Fq%3D$PARAMETERS',
-    400))
-  ->run();
-id(
-  new DaGdRegexTest(
-    '/c/store/g1/https://www.google.com/search%3Fq%3D$PARAMETERS',
-    '@Success@'))
-  ->setTolerateFailure(true)
-  ->run();
-id(
-  new DaGdRegexTest(
-    '/c/store/g1/https://www.google.com/search%3Fq%3D$PARAMETERS',
-    '@already been defined@'))
-  ->run();
-
+$runner->arm(
+  id(
+    new DaGdResponseCodeTest(
+      '/c/store/g/https://www.google.com/search?q=$PARAMETERS',
+      200))
+      ->setPreparatory(true)
+      ->setTolerateFailure(true));
+$runner->arm(
+  id(
+    new DaGdResponseCodeTest(
+      '/c/store/g/https://www.google.com/search?q=$PARAMETERS',
+      400)));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/c/store/g1/https://www.google.com/search?q=$PARAMETERS',
+      '@Success@'))
+      ->setPreparatory(true)
+      ->setTolerateFailure(true));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/c/store/g1/https://www.google.com/search?q=$PARAMETERS',
+      '@already been defined@')));
 
 /************ /c/xxxxxxx/xxxxxxx ************/
-id(new DaGdResponseCodeTest('/c/g/foobar', 302))
-  ->run();
-id(new DaGdResponseCodeTest('/c/g%20foobar', 302))
-  ->run();
-id(new DaGdResponseCodeTest('/c/nonexistent/foobar', 400))
-  ->run();
-id(new DaGdRegexTest('/c/nonexistent/foobar', '@was not found@'))
-  ->run();
+$runner->arm(
+  id(new DaGdResponseCodeTest('/c/g/foobar', 302)));
+$runner->arm(
+  id(new DaGdResponseCodeTest('/c/g%20foobar', 302)));
+$runner->arm(
+  id(new DaGdResponseCodeTest('/c/nonexistent/foobar', 400)));
+$runner->arm(
+  id(new DaGdRegexTest('/c/nonexistent/foobar', '@was not found@')));
 
 /************ /c/[xxxxxxx] ************/
-id(new DaGdRegexTest('/c/', '@Redirect@'))
-  ->run();
-id(new DaGdRegexTest('/c/json', '@{"g":@'))
-  ->run();
-id(new DaGdRegexTest('/c/json/', '@"g1":@'))
-  ->run();
-id(new DaGdContentTypeTest('/c/json/', 'application/json'))
-  ->setAccept('text/plain')
-  ->run();
-id(new DaGdContentTypeTest('/c/json', 'application/json'))
-  ->setAccept('text/html')
-  ->run();
-id(new DaGdContentTypeTest('/c', 'text/html'))
-  ->setAccept('text/html')
-  ->run();
-id(new DaGdContentTypeTest('/c/', 'text/html'))
-  ->setAccept('text/html')
-  ->run();
-id(new DaGdContentTypeTest('/c', 'text/plain'))
-  ->setAccept('text/plain')
-  ->run();
-id(new DaGdContentTypeTest('/c/', 'text/plain'))
-  ->setAccept('text/plain')
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/c/', '@Redirect@')));
+$runner->arm(
+  id(new DaGdRegexTest('/c/json', '@{"g":@')));
+$runner->arm(
+  id(new DaGdRegexTest('/c/json/', '@"g1":@')));
+$runner->arm(
+  id(new DaGdContentTypeTest('/c/json/', 'application/json'))
+  ->setAccept('text/plain'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/c/json', 'application/json'))
+  ->setAccept('text/html'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/c', 'text/html'))
+  ->setAccept('text/html'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/c/', 'text/html'))
+  ->setAccept('text/html'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/c', 'text/plain'))
+  ->setAccept('text/plain'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/c/', 'text/plain'))
+  ->setAccept('text/plain'));
 
 /************ /image/xxxxxxx/[xxxxxxx] ************/
-id(new DaGdContentTypeTest('/image/200x200', 'image/png'))
-  ->setAccept('text/html')
-  ->run();
-id(new DaGdContentTypeTest('/image/200x200', 'image/png'))
-  ->setAccept('text/plain')
-  ->run();
-id(new DaGdContentTypeTest('/image/10x10.jpg', 'image/jpeg'))
-  ->setAccept('text/plain')
-  ->run();
-id(new DaGdContentTypeTest('/image/30x20.gif?bgcolor=333333', 'image/gif'))
-  ->setAccept('text/html')
-  ->run();
-id(new DaGdResponseCodeTest('/image/300', 400))
-  ->run();
-id(new DaGdResponseCodeTest('/image/300000000x1212121221', 400))
-  ->run();
+$runner->arm(
+  id(new DaGdContentTypeTest('/image/200x200', 'image/png'))
+  ->setAccept('text/html'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/image/200x200', 'image/png'))
+  ->setAccept('text/plain'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/image/10x10.jpg', 'image/jpeg'))
+  ->setAccept('text/plain'));
+$runner->arm(
+  id(new DaGdContentTypeTest('/image/30x20.gif?bgcolor=333333', 'image/gif'))
+  ->setAccept('text/html'));
+$runner->arm(
+  id(new DaGdResponseCodeTest('/image/300', 400)));
+$runner->arm(
+  id(new DaGdResponseCodeTest('/image/300000000x1212121221', 400)));
 
 /************ /status/xxxxxxx/[xxxxxxx] ************/
-id(new DaGdResponseCodeTest('/status/400', 400))
-  ->run();
-id(new DaGdResponseCodeTest('/status/403', 403))
-  ->run();
-id(new DaGdResponseCodeTest('/status/321/hi', 321))
-  ->run();
+$runner->arm(
+  id(new DaGdResponseCodeTest('/status/400', 400)));
+$runner->arm(
+  id(new DaGdResponseCodeTest('/status/403', 403)));
+$runner->arm(
+  id(new DaGdResponseCodeTest('/status/321/hi', 321)));
 
 /************ /isp/[xxxxxxx] ************/
-id(new DaGdRegexTest(
-  '/isp/127.0.0.1',
-  '@^Internet Assigned Numbers Authority@'))
-  ->run();
-id(new DaGdRegexTest('/isp/69.171.237.16', '@^Facebook, Inc\.$@'))
-  ->run();
-id(new DaGdRegexTest('/isp/98.27.111.11', '@^Time Warner Cable Internet LLC$@'))
-  ->run();
-id(
-  new DaGdRegexTest(
-    '/isp/2001:470:8:624:211c:aaaa:1111:1111',
-    '@Network Operations@'))
-  ->run();
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/isp/127.0.0.1',
+      '@^Internet Assigned Numbers Authority@')));
+$runner->arm(
+  id(new DaGdRegexTest('/isp/69.171.237.16', '@Facebook, Inc\.@')));
+$runner->arm(
+  id(new DaGdRegexTest('/isp/1.1.1.1', '@Cloudflare@')));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/isp/2001:470:8:624:211c:aaaa:1111:1111',
+      '@Tunnelbroker@')));
 
 /************ /help ************/
-id(new DaGdRegexTest('/help', '@pixels: /image/200x400/png@'))
-  ->setAccept('text/html')
-  ->run();
-id(new DaGdRegexTest(
-  '/help?url_prefix=dagd%20&url_separator=%20&url_request_sep=%20--',
-  '@image 200x400 png@'))
-  ->setAccept('text/plain')
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/help', '@pixels: /image/200x400/png@'))
+  ->setAccept('text/html'));
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/help?url_prefix=dagd%20&url_separator=%20&url_request_sep=%20--',
+      '@image 200x400 png@'))
+      ->setAccept('text/plain'));
 
 /************ /dns/[xxxxxxx] ************/
-id(new DaGdRegexTest(
+$runner->arm(
+  id(new DaGdRegexTest(
   '/dns/google.com',
-  '@IN NS@'))
-  ->run();
+  '@IN NS@')));
 
-id(new DaGdRegexTest(
+$runner->arm(
+  id(new DaGdRegexTest(
   '/dns/google.com',
-  '@IN A@'))
-  ->run();
+  '@IN A@')));
 
 /************ /s/[xxxxxxx] ************/
-id(new DaGdRegexTest(
-  '/s?url=http://google.com/&shorturl=g',
-  '@/g@'))
-  ->setTolerateFailure(true)
-  ->run();
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/s?url=http://google.com/&shorturl=g',
+      '@/g@'))
+      ->setPreparatory(true)
+      ->setTolerateFailure(true));
 
-id(new DaGdRegexTest(
-  '/s?url=http://facebook.com/&shorturl=fbook',
-  '@/fbook@'))
-  ->setTolerateFailure(true)
-  ->run();
+$runner->arm(
+  id(
+    new DaGdRegexTest(
+      '/s?url=http://facebook.com/&shorturl=fbook',
+      '@/fbook@'))
+      ->setPreparatory(true)
+      ->setTolerateFailure(true));
 
-id(new DaGdResponseCodeTest('/g', 302))
-  ->run();
+$runner->arm(
+  id(new DaGdResponseCodeTest('/g', 302)));
 
-id(new DaGdResponseCodeTest('/g/foo', 302))
-  ->run();
+$runner->arm(
+  id(new DaGdResponseCodeTest('/g/foo', 302)));
 
-id(new DaGdResponseCodeTest('/fbook', 302))
-  ->run();
+$runner->arm(
+  id(new DaGdResponseCodeTest('/fbook', 302)));
 
 /************ ?strip ************/
-id(new DaGdRegexTest('/ip', '@\n$@'))
-  ->run();
-id(new DaGdRegexTest('/ip?strip=0', '@\n$@'))
-  ->run();
-id(new DaGdRegexTest('/ip?strip', '@[0-9]$@'))
-  ->run();
-id(new DaGdRegexTest('/ip?strip=1', '@[0-9]$@'))
-  ->run();
+$runner->arm(
+  id(new DaGdRegexTest('/ip', '@\n$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/ip?strip=0', '@\n$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/ip?strip', '@[0-9]$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/ip?strip=1', '@[0-9]$@')));
 
-$results = DaGdTest::getResultsSummary();
-echo chr(27)."[1;32m Passed        : ".$results['pass'].chr(27)."[0m"."\n";
-echo chr(27)."[1;31m Failed        : ".$results['fail'].chr(27)."[0m"."\n";
-echo chr(27)."[1;33m Tolerated Fail: ".$results['tolerated fail'].chr(27)."[0m".
-  "\n";
+/************ /roll/ ************/
+$runner->arm(
+  id(new DaGdResponseCodeTest('/roll/3d', 404)));
+$runner->arm(
+  id(new DaGdRegexTest('/roll/d9', '@^[0-9]$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/roll/3d1', '@^3$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/roll/3d1+3', '@^6$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/roll/3d1-1', '@^2$@')));
+$runner->arm(
+  id(new DaGdRegexTest('/roll/3d10', '@^\d+$@')));
 
-if ($results['fail'] === 0) {
-  exit(0);
-} else {
-  exit(1);
-}
+$runner->run();
+
+echo chr(27)."[1;32m Passed        : ".$runner->getPasses().chr(27)."[0m"."\n";
+echo chr(27)."[1;31m Failed        : ".$runner->getFailures().chr(27)."[0m"."\n";
+echo chr(27)."[1;33m Tolerated Fail: ".$runner->getToleratedFailures().chr(27).
+  "[0m"."\n";
+
+exit($runner->getReturnCode());
