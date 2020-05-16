33a540e37e71c160445757f889c46845ca35d43f
Rick Elrod <rick@elrod.me>
2020-05-16 01:18:45 -0500

    bump a statsd metric on unhandled exception
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- src/resources/global_resources.php
+++ src/resources/global_resources.php
@@ -25,6 +25,7 @@ spl_autoload_register('__dagd_autoload', $throw = true);
 
 include_once(dirname(__FILE__).'/tag.php');
 include_once(dirname(__FILE__).'/cowsay.php');
+include_once(dirname(__FILE__).'/statsd.php');
 
 $display_errors = DaGdConfig::get('general.display_errors');
 if ($display_errors) {
@@ -51,6 +52,7 @@ function handle_exception($e) {
     }
   }
 
+  statsd_bump('dagd_unhandled_exception');
   echo 'An error has occurred within dagd! Sorry about that!';
   header('HTTP/1.0 500 Internal Server Error (Exception)');
 
@@ -208,43 +210,6 @@ function client_ip() {
   } else {
     return $_SERVER['REMOTE_ADDR'];
   }
-}
-
-/** Bump statsd stat. */
-function statsd_send($name, $inc=1, $metric='c') {
-  $statsd_enabled = DaGdConfig::get('general.statsd');
-  $statsd_host = DaGdConfig::get('general.statsd_host');
-  $statsd_port = DaGdConfig::get('general.statsd_port');
-  $statsd_prefix = DaGdConfig::get('general.statsd_prefix');
-
-  if (!$statsd_enabled) {
-    return false;
-  }
-
-  $payload = $statsd_prefix.$name.':'.$inc.'|'.$metric;
-
-  try {
-    $sock = fsockopen('udp://'.$statsd_host, $statsd_port, $errno, $errstr);
-    fwrite($sock, $payload);
-    fclose($sock);
-    return true;
-  } catch (Exception $e) {
-    // Rethrow the exception if we are in debug mode.
-    $debug = DaGdConfig::get('general.debug');
-    debug('statsd_send', $e->getMessage());
-    if ($debug) {
-      throw $e;
-    }
-    return false;
-  }
-}
-
-function statsd_bump($name, $inc=1) {
-  return statsd_send($name, $inc, 'c');
-}
-
-function statsd_time($name, $time) {
-  return statsd_send($name, $time, 'ms');
 }
 
 /** Get help for a given class. */
