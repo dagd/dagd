f1215ecdceb1acf96b0f350b23a75eaab422c611
Rick Elrod <rick@elrod.me>
2020-06-04 20:33:36 -0500

    [container] Implement custom TCP poll for testing DB readiness, since `nc` is seemingly broken (segfaults) on centos:7 aarch64
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 set -x
 
+function is_db_live {
+  # `nc` seems to segfault on the centos7 aarch64 container image for some
+  # reason.
+  # nc -z db 3306
+  php <<'EOD'
+<?php
+$errno = null;
+$errstr = null;
+$res=@fsockopen("db", 3306, $errno, $errstr, 0.5);
+if ($res) {
+  fclose($res);
+  exit(0);
+} else {
+  exit(1);
+}
+EOD
+}
+
 i=0
 
-nc -z db 3306
+is_db_live
 r=$?
 
 while [[ $r -ne 0 ]]; do
   i=$((i+1))
   sleep 1
-  nc -z db 3306
+  is_db_live
   r=$?
   if [[ $i -ge 30 ]]; then
      echo "Could not connect to mysql within 30 seconds, failing."
