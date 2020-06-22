0d09d86b15f5ce78aadbec8f9a62c2f945b93413
Rick Elrod <rick@elrod.me>
2020-06-21 21:15:26 -0500

    [tests] Add some testing around sessions (both for darkmode and session decryption)
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ tests/test_session_regex.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once dirname(__FILE__).'/base.php';
+require_once dirname(dirname(__FILE__)).'/src/resources/global_resources.php';
+
+final class DaGdSessionRegexTest extends DaGdTest {
+  private $regex;
+  private $invert;
+
+  public function __construct(
+    $test_path,
+    $key,
+    $pattern,
+    $invert = false) {
+
+    $this->path = $test_path;
+    $this->key = $key;
+    $this->regex = $pattern;
+    $this->invert = $invert;
+  }
+
+  public function run() {
+    $content = $this->retrieve(true);
+    $headers = $this->getHeaders();
+    $session_data = '';
+
+    foreach ($headers as $k => $v) {
+      if ($k == 'Set-Cookie') {
+        $parsed = array();
+        parse_str(
+          strtr(
+            $v,
+            array(
+              '&' => '%26',
+              '+' => '%2B',
+              ';' => '&'
+            )
+          ),
+          $parsed);
+        foreach ($parsed as $ck => $cv) {
+          if (strpos($ck, 'DaGdSession_') === 0) {
+            $session_data .= $cv;
+          }
+        }
+      }
+    }
+
+    $session = id(new DaGdSession())->loadFromCookies($session_data);
+    $value = $session->get($this->key);
+
+    if (!$value) {
+      return $this->test(false, 'session key "'.$key.'" must exist');
+    }
+
+    if ($this->invert) {
+      $match = !preg_match($this->regex, $value);
+      return $this->test(
+        $match,
+        'session value for "'.$this->key.'" must NOT match: '.$this->regex,
+        $value);
+    } else {
+      $match = preg_match($this->regex, $value);
+      return $this->test(
+        $match,
+        'session value for "'.$this->key.'" must match: '.$this->regex,
+        $value);
+    }
+  }
+}
