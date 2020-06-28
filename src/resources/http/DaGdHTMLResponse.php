8f2f5f1f96941321265474bb9be7505f36ba4703
Rick Elrod <rick@elrod.me>
2020-06-28 13:30:53 -0500

    More useful error in DaGdHTMLResponse
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- src/resources/http/DaGdHTMLResponse.php
+++ src/resources/http/DaGdHTMLResponse.php
@@ -12,9 +12,18 @@ final class DaGdHTMLResponse extends DaGdResponse {
   public function getBody() {
     $body = parent::getBody();
     if (!($body instanceof Tag)) {
+      $cls = 'non-object';
+      if (is_string($body)) {
+        $cls = 'string';
+      } else {
+        $get_cls = get_class($body);
+        if ($cls !== false) {
+          $cls = $get_cls;
+        }
+      }
       throw new Exception(
         'Attempt to render DaGdHTMLResponse but body was not an instance of '.
-        'Tag. Did you mean to use DaGdHTMLStringResponse instead?');
+        'Tag. Got a '.$cls.' instead.');
     }
     $body = "<!doctype html>\n".$body->renderSafe();
     return $body;
