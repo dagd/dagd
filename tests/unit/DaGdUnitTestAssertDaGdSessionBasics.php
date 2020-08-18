dbd069ac1d864469b65d10aeae2fc365219e2c04
Rick Elrod <rick@elrod.me>
2020-08-18 12:47:57 -0500

    dagd-test: add some basic DaGdSession tests
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ tests/unit/DaGdUnitTestAssertDaGdSessionBasics.php
@@ -0,0 +1,35 @@
+<?php
+
+final class DaGdUnitTestAssertDaGdSessionBasics extends DaGdUnitTest {
+  public function runUnits() {
+    $this->path = 'DaGdUnitTestAssertDaGdSessionBasics';
+
+    $session = new DaGdSession();
+    $session->set('foo', 'bar');
+    $session->set('hey', 'hi');
+    $encrypted = $session->emit();
+    $first = $encrypted['DaGdSession_0'];
+    $new_session = id(new DaGdSession())
+      ->loadFromCookies($first);
+
+    $this->assertTrue(
+      $new_session->get('foo') === 'bar',
+      'set-get returns expected');
+
+    $this->assertTrue(
+      $new_session->get('hey') === 'hi',
+      'set-get returns expected');
+
+    $session->remove('foo');
+
+    // Re-emit the session
+    $encrypted = $session->emit();
+    $first = $encrypted['DaGdSession_0'];
+    $new_session = id(new DaGdSession())
+      ->loadFromCookies($first);
+
+    $this->assertTrue(
+      $new_session->get('foo', 'does not exist') === 'does not exist',
+      'set-get returns expected');
+  }
+}
