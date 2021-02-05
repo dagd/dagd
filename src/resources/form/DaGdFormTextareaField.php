55b154da057d170d20b2f582a624d98de7b5f71c
Rick Elrod <rick@elrod.me>
2021-02-05 14:04:35 -0600

    [DaGdFormTextareaField] Render field errors
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- src/resources/form/DaGdFormTextareaField.php
+++ src/resources/form/DaGdFormTextareaField.php
@@ -2,9 +2,29 @@
 
 class DaGdFormTextareaField extends DaGdFormField {
   public function toTag() {
-    return tag(
+    $error_lis = array();
+    foreach ($this->getErrors() as $error) {
+      $error_lis[] = tag('li', $error);
+    }
+
+    $error_ul = null;
+    if (!empty($error_lis)) {
+      $error_ul = tag('ul', $error_lis);
+    }
+
+    $field = tag(
       'textarea',
       $this->getValue() ? $this->getValue() : '',
       $this->getAttributes());
+
+    $div = tag(
+      'div',
+      array(
+        $field,
+        $error_ul,
+      )
+    );
+
+    return $div;
   }
 }
