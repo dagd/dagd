13a7ef69afcd17e140f18f2201c65b0028a36709
Daniel Lamando <dan@danopia.net>
2022-08-29 16:51:54 +0000

    Redescribe ISP application with newhelp API

--- src/applications/isp/DaGdISPController.php
+++ src/applications/isp/DaGdISPController.php
@@ -2,17 +2,27 @@
 
 final class DaGdISPController extends DaGdController {
   public static function getHelp() {
-    return array(
-      'title' => 'isp',
-      'summary' => 'Return the name of your ISP, or that of the given IP.',
-      'path' => 'isp',
-      'examples' => array(
-        array(
-          'summary' => 'Return the name of your ISP'),
-        array(
-          'arguments' => array('69.171.234.21'),
-          'summary' => 'Return the name of the ISP of the given address.'),
-      ));
+    return id(new DaGdHelp())
+      ->setTitle('isp')
+      ->setDescription('Returns the name of your ISP, or that of the given IP.')
+      ->addPath(
+        id(new DaGdHelpPath())
+          ->setPath('isp')
+          ->setMethods(array('GET'))
+          ->addExample(
+            id(new DaGdHelpExample())
+              ->setCommentary(
+                'Return the name of your ISP.')))
+      ->addPath(
+        id(new DaGdHelpPath())
+          ->setPath('isp')
+          ->addPathArg('ip')
+          ->setMethods(array('GET'))
+          ->addExample(
+            id(new DaGdHelpExample())
+              ->addPathArg('69.171.234.21')
+              ->setCommentary(
+                'Return the name of the ISP of the given address.')));
   }
 
   public function execute(DaGdResponse $response) {
