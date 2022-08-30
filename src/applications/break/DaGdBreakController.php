31edc05ce52bb6383ea33e7a4c5a8db8d2fa76db
Daniel Lamando <dan@danopia.net>
2022-08-31 00:26:56 +0200

    Update multi-line syntax
    Co-authored-by: Rick Elrod <rick@elrod.me>

--- src/applications/break/DaGdBreakController.php
+++ src/applications/break/DaGdBreakController.php
@@ -4,8 +4,9 @@ final class DaGdBreakController extends DaGdController {
   public function execute(DaGdResponse $response) {
     $environment = DaGdConfig::get('general.environment');
     if ($environment != 'development') {
-      return $this->error(400,
-        'This page is disabled in the production environment.')->execute($response);
+      return $this
+        ->error(400, 'This page is disabled in the production environment.')
+        ->execute($response);
     } else {
       throw new Exception('This is a test exception.');
     }
